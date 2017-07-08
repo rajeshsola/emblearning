0997e46c9bb745e5f3167defe8fd5628a54049f7
Rajesh Sola <rajeshsola@gmail.com>
2017-07-08 22:28:59 +0530

    Update hello.c

--- ldd-examples/simple-modules/hello/hello.c
+++ ldd-examples/simple-modules/hello/hello.c
@@ -2,8 +2,6 @@
 #include<linux/module.h>
 int __init hello_init(void)	//init_module
 {
-	pid_t pid;
-	pid_t ppid;
 	printk("Hello module::init--welcome\n");
 	return 0;
 }
@@ -13,6 +11,6 @@ void __exit hello_exit(void) 	//cleanup_module
 }
 module_init(hello_init);
 module_exit(hello_exit);
-MODULE_AUTHOR("Your Name");
+MODULE_AUTHOR("Rajesh Sola");
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("A Simple Module");
+MODULE_DESCRIPTION("A Simple Hello Module");
