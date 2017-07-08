70a3271467598a9618821636fe0e3a9b91447999
Rajesh Sola <rajeshsola@gmail.com>
2017-07-08 22:30:29 +0530

    Update hello.c

--- ldd-examples/simple-modules/logdemo/hello.c
+++ ldd-examples/simple-modules/logdemo/hello.c
@@ -2,15 +2,16 @@
 #include<linux/module.h>
 int __init hello_init(void)	//init_module
 {
-	printk("Hello module::init--welcome\n");
-	printk(KERN_EMERG   "Hello module::emergency\n"); //<0>
-	printk(KERN_ALERT   "Hello module::alert\n"); //<1>
-	printk(KERN_CRIT    "Hello module::critical\n"); //<2>
-	printk(KERN_ERR     "Hello module::error\n"); //<3>
-	printk(KERN_WARNING "Hello module::warning\n");//<4>
-	printk(KERN_NOTICE  "Hello module::notice\n");//<5>
-	printk(KERN_INFO    "Hello module::info\n");//<6>
-	printk(KERN_DEBUG   "Hello module::debug\n");//<7>
+	printk("Hello module::init--welcome\n"); //Default log level KERN_WARNING <4>
+	printk(KERN_EMERG   "Hello module::emergency\n"); 	//<0>
+	printk(KERN_ALERT   "Hello module::alert\n"); 		//<1>
+	printk(KERN_CRIT    "Hello module::critical\n"); 	//<2>
+	printk(KERN_ERR     "Hello module::error\n"); 		//<3>
+	printk(KERN_WARNING "Hello module::warning\n");		//<4>
+	printk(KERN_NOTICE  "Hello module::notice\n");		//<5>
+	printk(KERN_INFO    "Hello module::info\n");		//<6>
+	printk(KERN_DEBUG   "Hello module::debug\n");		//<7>
+	printk(KERN_DEFAULT   "Hello module::default\n");	//<4>
 	return 0;
 }
 void __exit hello_exit(void) 	//cleanup_module
@@ -19,6 +20,6 @@ void __exit hello_exit(void) 	//cleanup_module
 }
 module_init(hello_init);
 module_exit(hello_exit);
-MODULE_AUTHOR("Your Name");
+MODULE_AUTHOR("Rajesh Sola");
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("A Simple Module");
